0bf1e9f19fb11d31ff01de2ee192a60746931b31
Aditya <kavsrkrishna200@gmail.com>
2023-06-04 03:34:22 +0000

    q!

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-	System.out.println("Welcome Git from clone Multipe!!!");
+	System.out.println("Welcome Git from clone Krishna  Multipe!!!");
     }
 }
